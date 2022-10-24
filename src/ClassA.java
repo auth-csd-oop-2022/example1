2406cab808dccd8c04ef68c9adafa35f706ac17b
George Meditskos <gmeditsk@csd.auth.gr>
2022-10-25 00:06:47 +0300

    typo

--- src/ClassA.java
+++ src/ClassA.java
@@ -2,7 +2,7 @@ public class ClassA {
 
     public ClassA(int a, int b, int c, int d, int e) {
         /**
-         * We use the constructor of ClassB for the three parameters, and then we use the setter for the other two,
+         * We use the constructor of ClassB for the three parameters, and then we use the setters for the other two,
          * since there is no constructor for ClassB that takes 5 parameters.
          */
         ClassB instance = new ClassB(a, b, c);
